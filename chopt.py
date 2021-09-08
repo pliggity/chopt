9eb6b4e08dbdb90d64c303d85a1afe29712cf17a
Mariah Arnold <pliggity@Mariahs-MacBook-Pro.local>
2021-09-08 16:25:00 -0700

    Adding more ingredients

--- chopt.py
+++ chopt.py
@@ -12,7 +12,7 @@ __license__ = "MIT"
 
 chefs = ["Austin", "Amanda", "Darien", "Mariah", "Rico","Taylor"]
 courses = ["Drinks", "Appetizer 1", "Main Course", "Dessert", "Side Dish", "Appetizer 2"]
-ingredients = ["Avocado", "Mint", "Coffee", "Cilantro", "Quail Eggs", "Basil", "Salmon"]
+ingredients = ["Avocado", "Mint", "Coffee", "Cilantro", "Quail Eggs", "Basil", "Salmon", "Coconut Milk", "Lemongrass", "Pringles", "Duck Fat", "Pickle Juice", "Rice", "Carrot", "Honey", "Takis", "Eggplant", "Peanut Butter", "Lemon Curd", "Fish Eggs (of your choice)", "Rum", "Maple Syrup", "Star Fruit", "Okra", "Gizzards", "Champagne grapes", "Gold", "Kool Aid", "Sriracha", "Liquid Smoke", "Biscoff Cookies", "Creme de Menthe", "Pork Rinds", "Everything Bagil Seasoning", "Miso"]
 
 def chopt_assign():
 
