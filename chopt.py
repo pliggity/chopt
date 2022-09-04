220b6e84c4bf685246e762fa072a52d2279c8cae
pliggity <mariahmay93@gmail.com>
2022-09-03 19:16:12 -0700

    Update program to include new chefs and fair themed food

--- chopt.py
+++ chopt.py
@@ -10,9 +10,10 @@ __author__ = "pliggity"
 __version__ = "0.1"
 __license__ = "MIT"
 
-chefs = ["Austin", "Amanda", "Darien", "Mariah", "Rico","Taylor"]
-courses = ["Drinks", "Appetizer 1", "Main Course", "Dessert", "Side Dish", "Appetizer 2"]
-ingredients = ["Avocado", "Mint", "Coffee", "Cilantro", "Quail Eggs", "Basil", "Salmon", "Coconut Milk", "Lemongrass", "Pringles", "Duck Fat", "Pickle Juice", "Rice", "Carrot", "Honey", "Takis", "Eggplant", "Peanut Butter", "Lemon Curd", "Fish Eggs (of your choice)", "Rum", "Maple Syrup", "Star Fruit", "Okra", "Gizzards", "Champagne grapes", "Gold", "Kool Aid", "Sriracha", "Liquid Smoke", "Biscoff Cookies", "Creme de Menthe", "Pork Rinds", "Everything Bagil Seasoning", "Miso"]
+chefs = ["Austin", "Amanda", "Darien", "Mariah", "Ryan","Taylor", "Sarah"]
+courses = ["Beverage 1", "Beverage 2", "Appetizer 1", "Main Course", "Dessert", "Side Dish", "Appetizer 2"]
+ingredients = ["Lemon", "Lime","Cinnamon","Graham cracker","Corn","Powdered sugar","Potatoes","Ground meat","Beer","Twinkies","Onion","Strawberries","Stick o' butter","Bacon","Mushrooms","Marshmallow",
+"Chocolate","Peach","Pizza","Turkey leg","Oreos", "Red Bull","Cheese curds","Corn bread","Hot dog","Panko","Radishes", "Pumpkin","Squash","Caramel apple"]
 
 def chopt_assign():
 
