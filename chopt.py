dda6e35d2e76559e42f65fcca6becbb81d3c58db
pliggity <mariahmay93@gmail.com>
2023-09-09 14:19:36 -0700

    Add tv show inspiration for 2023

--- chopt.py
+++ chopt.py
@@ -10,12 +10,24 @@ __author__ = "pliggity"
 __version__ = "0.1"
 __license__ = "MIT"
 
-chefs = ["Austin", "Amanda", "Darien", "Mariah", "Ryan","Taylor", "Sarah"]
-courses = ["Beverage 1", "Beverage 2", "Appetizer 1", "Main Course", "Dessert", "Side Dish", "Appetizer 2"]
+chefs = ["Austin", "Amanda", "Darien", "Maryan", "Taylor"]
+courses = ["Beverage", "Main Course", "Dessert", "Side", "Appetizer"]
 ingredients = ["Lemon", "Lime","Cinnamon","Graham cracker","Corn","Powdered sugar","Potatoes","Ground meat","Beer","Twinkies","Onion","Strawberries","Stick o' butter","Bacon","Mushrooms","Marshmallow",
 "Chocolate","Peach","Pizza","Turkey leg","Oreos", "Red Bull","Cheese curds","Corn bread","Hot dog","Panko","Radishes", "Pumpkin","Squash","Caramel apple"]
 
-def chopt_assign():
+# opening the file in read mode
+tv_shows_file = open("tv_shows.txt", "r")
+  
+# reading the file
+data = tv_shows_file.read()
+  
+# replacing end splitting the text 
+# when newline ('\n') is seen.
+data_into_list = data.split("\n")
+tv_shows = list(set(data_into_list))
+tv_shows_file.close()
+
+def chopt_assign_ingredient():
 
     random.seed()
 
@@ -29,8 +41,21 @@ def chopt_assign():
         ingredients.pop(i)
 
 
+def chopt_assign_tv_show():
+    random.seed()
+    for chef in chefs:
+        c = randrange(len(courses))
+        i = randrange(len(tv_shows))
+
+        print(chef, " - ", courses[c], " - ", tv_shows[i], "\n")
+
+        courses.pop(c)
+        tv_shows.pop(i)
+
+
 def main():
-    chopt_assign()
+    chopt_assign_tv_show()
+    #chopt_assign()
 
 
     
@@ -39,4 +64,3 @@ def main():
 if __name__ == "__main__":
     """ This is executed when run from the command line """
     main()
-
